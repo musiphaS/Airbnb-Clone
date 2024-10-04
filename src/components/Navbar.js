268cb907e146f41846512be04021f96f8ca948e2
Kelvin <kelvinmusipha@gmail.com>
2024-10-04 11:21:03 +0200

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import image from "../images/airbnb-logo.png"
 export default function Navbar() {
     return (
         <nav className="nav--logo">
-            <img src={image} alt="hyu"/>
+            <img src={image} alt="hyu" className="navbar-pic"/>
         </nav>
     )
 }
